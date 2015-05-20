e27d3b8647f86f01950fd12080903ed3725eb4f5
Johnny Chang <johnny@johnnychang.com>
2015-05-20 13:56:19 -0700

    added a working sample with CMU Pronouncing Dict and a markdown file for setting up/executing

---
+++ dependencies.md
@@ -0,0 +1,12 @@
+#Dependencies
+
+##My setup:
+- Python 2.7.6
+- NLTK 3.0.2 (latest release)
+
+##How to run:
+`./rhyme_detect.py`
+*or*
+`python rhyme_detect.py`
+
+NB: the python has `#!/usr/bin/python` at the top to tell the kernel where the program is to run it (using `./rhyme_detect.py`). This is the *default installation path*, if you chose a different installation directory then you should just use `python rhyme_detect.py`
--- rhyme_detect.py
+++ rhyme_detect.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python
 
 import nltk
-import nltk.corpus.reader.cmudict
+from nltk.corpus import cmudict
 
 # Sample rhymes
 # Lose Yourself - Eminem
 
-lose_yourself = "His palms are sweaty, knees weak, arms are heavy
-        There's vomit on his sweater already, mom's spaghetti
-        He's nervous, but on the surface he looks calm and ready to drop bombs,
-        But he keeps on forgetting what he wrote down,
-        The whole crowd goes so loud
-        He opens his mouth, but the words won't come out"
+#lose_yourself = "His palms are sweaty, knees weak, arms are heavy
+#        There's vomit on his sweater already, mom's spaghetti
+#        He's nervous, but on the surface he looks calm and ready to drop bombs,
+#        But he keeps on forgetting what he wrote down,
+#        The whole crowd goes so loud
+#        He opens his mouth, but the words won't come out"
+
+perfect_rhyme = "This is a perfect rhyme \n bitches turn on a dime"
+
+sample_text = "Mackerel bat from hell"
+
+transcr = cmudict.dict()
+
+results = [transcr[w][0] for w in sample_text.lower().split()]
+
+print(results)
 
 
