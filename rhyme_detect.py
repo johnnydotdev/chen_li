371b194cd33735d1f75a7e075d815899b0e54fa6
Johnny Chang <johnny@johnnychang.com>
2015-05-19 20:09:31 -0700

    initial commit

---
+++ rhyme_detect.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import nltk
+import nltk.corpus.reader.cmudict
+
+# Sample rhymes
+# Lose Yourself - Eminem
+
+lose_yourself = "His palms are sweaty, knees weak, arms are heavy
+        There's vomit on his sweater already, mom's spaghetti
+        He's nervous, but on the surface he looks calm and ready to drop bombs,
+        But he keeps on forgetting what he wrote down,
+        The whole crowd goes so loud
+        He opens his mouth, but the words won't come out"
+
+
