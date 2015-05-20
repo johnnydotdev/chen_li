dda3002269065790f5c7ab6b11f008622f321e11
Johnny Chang <johnny@johnnychang.com>
2015-05-20 14:20:34 -0700

    made transcription a function

--- rhyme_detect.py
+++ rhyme_detect.py
@@ -3,26 +3,34 @@
 import nltk
 from nltk.corpus import cmudict
 
-# Sample rhymes
+# Sample rhymes as a string, or a list of strings
 # Lose Yourself - Eminem
 
-#lose_yourself = "His palms are sweaty, knees weak, arms are heavy
-#        There's vomit on his sweater already, mom's spaghetti
-#        He's nervous, but on the surface he looks calm and ready to drop bombs,
-#        But he keeps on forgetting what he wrote down,
-#        The whole crowd goes so loud
-#        He opens his mouth, but the words won't come out"
+LOSE_YOURSLEF = ["His palms are sweaty, knees weak, arms are heavy",
+                 "There's vomit on his sweater already, mom's spaghetti",
+                 "He's nervous, but on the surface he looks calm and ready to drop bombs",
+                 "But he keeps on forgetting what he wrote down",
+                 "The whole crowd goes so loud",
+                 "He opens his mouth, but the words won't come out"]
 
-perfect_rhyme = ["This is a perfect rhyme", "bitches split on a dime"]
+PERFECT_RHYME = ["This is a perfect rhyme", 
+                 "bitches split on a dime"]
 
-sample_text = "Mackerel bat from hell"
+SAMPLE_TEXT = "Mackerel bat from hell"
 
 transcr = cmudict.dict()
 
-# transcribes the sample string given to the CMU Dictionary.
-results = [transcr[w][0] for w in sample_text.lower().split()]
+## Description: transcribes a string into its phonemes and prints the result out.
+# param: string str to transcribe
+# return: list of lists of phonemes
 
-# let's see the output!
-print(results)
+def transcribe_string(string):
+    results = [transcr[w][0] for w in string.lower().split()]  # transcribes the sample string given to the CMU Dictionary.
 
+    return results
+
+print(transcribe_string(SAMPLE_TEXT)) # Prints out phonemes for "Mackerel bat from hell"
+
+for s in PERFECT_RHYME:
+    print(transcribe_string(s))
 
